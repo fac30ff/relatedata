aa01617807e68e328da1954f092b47955b5f7b17
IR <fac30ff@gmail.com>
2018-10-05 10:19:55 +0300

    rc collapse workable

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Collapse, { Panel } from 'rc-collapse';
+import 'rc-collapse/assets/index.css';
+import MonitoringConfig from './MonitoringConfig';
+
+function random() {
+  return parseInt(Math.random() * 10, 10) + 1;
+}
+
+const loadData = fetch('./MonitoringConfig.json')
+  .then(res => res.json());
+
+const fakeData = () => JSON.parse(JSON.stringify(MonitoringConfig));
 
 class App extends Component {
+  state = {
+    time: random(),
+    accordion: false,
+    activeKey: ['1'],
+  }
+
+  getItems() {
+    const items = [];
+    for (let i = 0, len = 2; i < len; i++) {
+      const key = i + 1;
+      items.push(
+        <Panel header={`This is panel header ${key}`} key={key} disabled={i === 3}>
+          <p>{"hello world".repeat(this.state.time)}</p>
+        </Panel>
+      );
+    }
+
+       return items;
+  }
+
+  onChange = (activeKey) => {
+    this.setState({
+      activeKey,
+    });
+  }
+
   render() {
+    const accordion = this.state.accordion;
+    const activeKey = this.state.activeKey;
     return (
       <div className="App">
         <header className="App-header">
@@ -20,6 +60,13 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <Collapse
+          accordion={accordion}
+          onChange={this.onChange}
+          activeKey={activeKey}
+        >
+          {this.getItems()}
+        </Collapse>
       </div>
     );
   }
